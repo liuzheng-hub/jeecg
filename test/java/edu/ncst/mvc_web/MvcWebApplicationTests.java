afe0bb8c767fc34ad8e80a89ccc5b29d80618239
liuzheng-hub <106644806+Changeshihcheng@users.noreply.github.com>
2022-06-01 14:21:07 +0800

    Create MvcWebApplicationTests.java

---
+++ test/java/edu/ncst/mvc_web/MvcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ncst.mvc_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
