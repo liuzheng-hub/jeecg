2e8dbe55f45877938c428fa051a1a44940e6a3f5
liuzheng-hub <106644374+zuiyufajlk@users.noreply.github.com>
2022-06-01 14:17:14 +0800

    Create MvcWebApplication.java

---
+++ .mvn/MvcWebApplication.java
@@ -0,0 +1,13 @@
+package edu.ncst.mvcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcWebApplication.class, args);
+    }
+
+}
